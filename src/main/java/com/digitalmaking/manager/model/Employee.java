b7694668a33e086f8557028a2e95f8b025407536
Tito <50543732+DevWebJr@users.noreply.github.com>
2023-01-08 18:04:48 +0100

    Create packages : Model, Repository, Service. Add some CRUD methods on them

---
+++ src/main/java/com/digitalmaking/manager/model/Employee.java
@@ -0,0 +1,14 @@
+package com.digitalmaking.manager.model;
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    private String employeeCode;
+
+    
+}
